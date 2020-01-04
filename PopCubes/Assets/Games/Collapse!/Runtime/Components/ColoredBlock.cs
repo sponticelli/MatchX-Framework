16968abee5600b944a0a981ece2c5f00e3c23e36
sponticelli <sandro.ponticelli@gmail.com>
2020-01-05 00:19:03 +0100

    Added tweens to colored blocks

--- PopCubes/Assets/Games/Collapse!/Runtime/Components/ColoredBlock.cs
+++ PopCubes/Assets/Games/Collapse!/Runtime/Components/ColoredBlock.cs
@@ -1,18 +1,34 @@
 ﻿using UnityEngine;
+using ZigZaggle.Core.Math;
+using ZigZaggle.Core.Tweening;
 
 namespace ZigZaggle.Collapse.Components
 {
     public class ColoredBlock : BaseBlock
     {
+        private TweenOperation movement;
         public override void Explode()
         {
             base.Explode();
-            Destroy(this.gameObject);
+            Tween.LocalScale(transform, Vector3.zero, 0.25f, 0, 
+                easeCurve: Easing.EaseInOutStrong,
+                completeCallback: () =>
+            {
+                Destroy(this.gameObject);
+            });
+            
         }
 
         public override void Move(Vector3 newPosition, float seconds)
         {
-            transform.position = newPosition;
+            if (movement != null && movement.Status == Tween.TweenStatus.Running)
+            {
+                movement = Tween.Position(transform, newPosition, seconds, movement.Duration);
+            }
+            else
+            {
+                movement = Tween.Position(transform, newPosition, seconds, 0);
+            }
         }
     }
 }
