9f2a98b3c5521b7acb73dffde8f66ffdd4d9b8a1
sponticelli <sandro.ponticelli@gmail.com>
2020-01-06 20:51:04 +0100

    Simple Sound Player

---
+++ PopCubes/Assets/Core/Runtime/Audio/SimpleSoundPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace ZigZaggle.Core.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SimpleSoundPlayer : MonoBehaviour
+    {
+        public static SimpleSoundPlayer Instance { get; private set; }
+
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            audioSource = GetComponent<AudioSource>();
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public static void Play(AudioClip clip)
+        {
+            if (clip != null)
+                Instance.audioSource.PlayOneShot(clip);
+        }
+    }
+}
